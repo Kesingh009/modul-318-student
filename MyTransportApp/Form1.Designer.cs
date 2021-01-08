e8a3eef713522c8375e50d3cff0494872c61c38e
Kevin Singh <Kevin Singh@DESKTOP-JJ7L6B2>
2021-01-08 15:39:59 +0100

    doc

--- MyTransportApp/Form1.Designer.cs
+++ MyTransportApp/Form1.Designer.cs
@@ -35,9 +35,6 @@ namespace MyTransportApp
             this.label3 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label4 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
@@ -47,6 +44,9 @@ namespace MyTransportApp
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -57,7 +57,7 @@ namespace MyTransportApp
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(140, 21);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Verbindung Suchen";
+            this.button1.Text = "Verbindung suchen";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -87,7 +87,7 @@ namespace MyTransportApp
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(315, 435);
+            this.label3.Location = new System.Drawing.Point(315, 452);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(293, 55);
             this.label3.TabIndex = 6;
@@ -95,6 +95,7 @@ namespace MyTransportApp
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column2,
@@ -109,6 +110,7 @@ namespace MyTransportApp
             // 
             // dataGridView2
             // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.dataGridViewTextBoxColumn1,
@@ -118,24 +120,7 @@ namespace MyTransportApp
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.Size = new System.Drawing.Size(689, 264);
             this.dataGridView2.TabIndex = 8;
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this.dataGridViewTextBoxColumn1.HeaderText = "Abfahrt";
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            this.dataGridViewTextBoxColumn1.Width = 200;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this.dataGridViewTextBoxColumn2.HeaderText = "Nach";
-            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            this.dataGridViewTextBoxColumn2.Width = 200;
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Dauer";
-            this.Column3.Name = "Column3";
-            this.Column3.Width = 200;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // label4
             // 
@@ -153,8 +138,9 @@ namespace MyTransportApp
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(140, 23);
             this.button2.TabIndex = 11;
-            this.button2.Text = "Verbindung Suchen";
+            this.button2.Text = "Verbindung suchen";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBox1
             // 
@@ -189,31 +175,56 @@ namespace MyTransportApp
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(220, 21);
             this.comboBox3.TabIndex = 14;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_TextChanged);
+            this.comboBox3.TextChanged += new System.EventHandler(this.comboBox3_TextChanged);
             // 
             // Column2
             // 
             this.Column2.HeaderText = "Dauer";
             this.Column2.Name = "Column2";
+            this.Column2.Width = 161;
             // 
             // Column1
             // 
             this.Column1.HeaderText = "Abfahrt";
             this.Column1.Name = "Column1";
+            this.Column1.Width = 161;
             // 
             // Column4
             // 
             this.Column4.HeaderText = "Verspätung";
             this.Column4.Name = "Column4";
+            this.Column4.Width = 161;
             // 
             // Column5
             // 
-            this.Column5.HeaderText = "Ankunft";
+            this.Column5.HeaderText = "Plattform";
             this.Column5.Name = "Column5";
+            this.Column5.Width = 161;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Abfahrt";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 214;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "Nach";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 214;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Name";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 214;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(932, 882);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
@@ -244,9 +255,6 @@ namespace MyTransportApp
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
@@ -256,6 +264,9 @@ namespace MyTransportApp
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
 
--- MyTransportApp/Form1.cs
+++ MyTransportApp/Form1.cs
@@ -24,11 +24,9 @@ namespace MyTransportApp
             var connections = transport.GetConnections(comboBox1.Text , comboBox2.Text);
             dataGridView1.Rows.Clear();
             dataGridView1.Rows.Add(connections.ConnectionList[0].Duration, connections.ConnectionList[0].From.Departure, connections.ConnectionList[0].From.Delay, connections.ConnectionList[0].From.Platform);
-            dataGridView1.Rows.Add(connections.ConnectionList[1].Duration, connections.ConnectionList[1].From.Departure, connections.ConnectionList[1].From.Delay);
-            dataGridView1.Rows.Add(connections.ConnectionList[2].Duration, connections.ConnectionList[2].From.Departure, connections.ConnectionList[2].From.Delay);
-            dataGridView1.Rows.Add(connections.ConnectionList[3].Duration, connections.ConnectionList[3].From.Departure, connections.ConnectionList[3].From.Delay);
-            
-
+            dataGridView1.Rows.Add(connections.ConnectionList[1].Duration, connections.ConnectionList[1].From.Departure, connections.ConnectionList[1].From.Delay, connections.ConnectionList[1].From.Platform);
+            dataGridView1.Rows.Add(connections.ConnectionList[2].Duration, connections.ConnectionList[2].From.Departure, connections.ConnectionList[2].From.Delay, connections.ConnectionList[2].From.Platform);
+            dataGridView1.Rows.Add(connections.ConnectionList[3].Duration, connections.ConnectionList[3].From.Departure, connections.ConnectionList[3].From.Delay, connections.ConnectionList[3].From.Platform);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -43,39 +41,99 @@ namespace MyTransportApp
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            var transport = new Transport();
-            var connections = transport.GetConnections("luzern","sursee");
-            var temp = connections.ConnectionList[0].From.Station;
+           
         }
 
         private void comboBox1_TextChanged(object sender, EventArgs e)
         {
             var transport = new Transport();
-            var stations = transport.GetStations(comboBox1.Text);
-            var temp = stations.StationList[0].Name;
-
-            foreach(var x in stations.StationList)
+            if (comboBox1.Text != "")
             {
-                comboBox1.Items.Add(x.Name);
+                var stations = transport.GetStations(comboBox1.Text);
+                var temp = stations.StationList[0].Name;
+
+                foreach (var x in stations.StationList)
+                {
+                    comboBox1.Items.Add(x.Name);
+                }
+
+
             }
-            
+            else 
+            {
+                comboBox1.Text = "";
+            }            
+
         }
 
         private void comboBox2_TextChanged(object sender, EventArgs e)
         {
             var transport = new Transport();
-            var stations = transport.GetStations(comboBox2.Text);
-            var temp = stations.StationList[0].Name;
-
-            foreach (var x in stations.StationList)
+            if (comboBox2.Text != "")
             {
-                comboBox2.Items.Add(x.Name);
+                var stations = transport.GetStations(comboBox2.Text);
+                var temp = stations.StationList[0].Name;
+
+                foreach (var x in stations.StationList)
+                {
+                    comboBox2.Items.Add(x.Name);
+                }
+
+
             }
+            else
+            {
+                comboBox2.Text = "";
+            }
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
            
+        }
+
+        private void comboBox3_TextChanged(object sender, EventArgs e)
+        {
+            var transport = new Transport();
+            if (comboBox3.Text != "")
+            {
+                var stations = transport.GetStations(comboBox3.Text);
+                var temp = stations.StationList[0].Name;
+
+                foreach (var x in stations.StationList)
+                {
+                    comboBox3.Items.Add(x.Name);
+                }
+
+
+            }
+            else
+            {
+                comboBox3.Text = "";
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var transport = new Transport();
+            var connections = transport.GetStationBoard(comboBox3.Text,comboBox3.Text);
+            dataGridView2.Rows.Clear();
+            dataGridView2.Rows.Add(connections.Entries[0].Stop.Departure, connections.Entries[0].To, connections.Entries[0].Name);
+            dataGridView2.Rows.Add(connections.Entries[1].Stop.Departure, connections.Entries[1].To, connections.Entries[1].Name);
+            dataGridView2.Rows.Add(connections.Entries[2].Stop.Departure, connections.Entries[2].To, connections.Entries[2].Name);
+            dataGridView2.Rows.Add(connections.Entries[3].Stop.Departure, connections.Entries[3].To, connections.Entries[3].Name);
+            dataGridView2.Rows.Add(connections.Entries[4].Stop.Departure, connections.Entries[4].To, connections.Entries[4].Name);
+            dataGridView2.Rows.Add(connections.Entries[5].Stop.Departure, connections.Entries[5].To, connections.Entries[5].Name);
+            dataGridView2.Rows.Add(connections.Entries[6].Stop.Departure, connections.Entries[6].To, connections.Entries[6].Name);
+            dataGridView2.Rows.Add(connections.Entries[7].Stop.Departure, connections.Entries[7].To, connections.Entries[7].Name);
+            dataGridView2.Rows.Add(connections.Entries[8].Stop.Departure, connections.Entries[8].To, connections.Entries[8].Name);
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+          
         }
     }   
 }
